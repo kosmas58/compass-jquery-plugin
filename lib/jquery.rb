323e49e14bfaa312e57bd826c3f4806a505e1046
kosmas58 <kosmas.schuetz@gmx.com>
2009-11-08 10:22:59 +0100

    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- lib/jquery.rb
+++ lib/jquery.rb
@@ -2,5 +2,9 @@ require File.join(File.dirname(__FILE__), 'jquery', 'compass_plugin')
 
 module CompassJqueryPlugin
   version = YAML.load_file(File.dirname(__FILE__) + '/../VERSION.yml')
-  VERSION = "#{version[:major]}.#{version[:minor]}.#{version[:patch]}"
+  if version[:commit]
+    VERSION = "#{version[:major]}.#{version[:minor]}.#{version[:patch]}.#{version[:commit]}" 
+  else
+    VERSION = "#{version[:major]}.#{version[:minor]}.#{version[:patch]}" 
+  end
 end
