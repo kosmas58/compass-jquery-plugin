bfb9de7d3e69afe8fe8eb2fa4ff50989a5b8e199
kosmas58 <kosmas.schuetz@gmx.com>
2010-02-22 20:37:37 +0100

    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- src/jrails.1.3.2/config/initializers/jrails.rb
+++ src/jrails.1.3.2/config/initializers/jrails.rb
@@ -14,10 +14,10 @@ ActionView::Helpers::PrototypeHelper::JQUERY_VAR = 'jQuery'
 # ActionView::Helpers::PrototypeHelper::DISABLE_JQUERY_FORGERY_PROTECTION = true
 # ====
 
-ActionView::Helpers::AssetTagHelper::JAVASCRIPT_DEFAULT_SOURCES = ['jquery.1.3.2.min', 'jquery-ui.1.7.2.min', 'jrails.min']
+ActionView::Helpers::AssetTagHelper::JAVASCRIPT_DEFAULT_SOURCES = ['jquery-1.3.2.min', 'jquery-ui-1.7.2.min', 'jrails.min']
 ActionView::Helpers::AssetTagHelper::reset_javascript_include_default
 
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ['jquery.1.3.2.min', 'jquery-ui.1.7.2.min']
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ['jquery-1.3.2.min', 'jquery-ui-1.7.2.min']
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jrails => ['jrails.min']
 
 #ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :jrails => ['compiled/jquery.ui/ui.theme.css']
--- src/jrails.1.4.1/config/initializers/jrails.rb
+++ src/jrails.1.4.1/config/initializers/jrails.rb
@@ -14,10 +14,11 @@ ActionView::Helpers::PrototypeHelper::JQUERY_VAR = 'jQuery'
 # ActionView::Helpers::PrototypeHelper::DISABLE_JQUERY_FORGERY_PROTECTION = true
 # ====
 
-ActionView::Helpers::AssetTagHelper::JAVASCRIPT_DEFAULT_SOURCES = ['jquery.1.4.1.min', 'jquery-ui.1.8rc1.min', 'jrails.min']
+ActionView::Helpers::AssetTagHelper::JAVASCRIPT_DEFAULT_SOURCES = ['jquery-1.4.1.min', 'jquery-ui-1.8rc1.min', 'jrails.min']
 ActionView::Helpers::AssetTagHelper::reset_javascript_include_default
 
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ['jquery.1.4.1.min', 'jquery-ui.1.8rc1.min']
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ['jquery-1.4.1.min', 'jquery-ui-1.8rc1.min']
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jrails => ['jrails.min']
 
 #ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :jrails => ['compiled/jquery.ui/ui.theme.css']
 #ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :jquery => ['compiled/jquery.ui/ui.theme.css']
