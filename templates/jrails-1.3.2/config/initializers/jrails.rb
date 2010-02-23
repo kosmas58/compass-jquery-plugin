1f99b36302eb2df91cfc38fa233810d4243ca3fe
kosmas58 <kosmas.schuetz@gmx.com>
2010-02-23 06:32:04 +0100

    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- templates/jrails-1.3.2/config/initializers/jrails.rb
+++ templates/jrails-1.3.2/config/initializers/jrails.rb
@@ -19,6 +19,8 @@ ActionView::Helpers::AssetTagHelper::reset_javascript_include_default
 
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ['jquery-1.3.2.min', 'jquery-ui-1.7.2.min']
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jrails => ['jrails.min']
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :contextMenu => ['jquery.contextMenu.min']
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :pngFix => ['jquery.pngFix.min']
 
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :contextMenu => ['compiled/jquery.ui/contextMenu.css']
 
