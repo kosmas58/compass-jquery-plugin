daaea6dcb868dd46014d2c5d60d396ee97d60d5c
kosmas58 <kosmas.schuetz@gmx.com>
2010-03-17 13:54:08 +0100

    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- templates/jrails-1.4/jquery.themeswitchertool.js
+++ templates/jrails-1.4/jquery.themeswitchertool.js
@@ -109,7 +109,7 @@ $.fn.themeswitcher = function(settings){
 		}
 	)
 	.find('.jquery-ui-themeswitcher-icon').css({
-		float: 'right',
+		'float': 'right',
 		width: '16px',
 		height: '16px',
 		background: 'url("/images/jquery.ui/themeSwitcher/icon_color_arrow.gif") 50% 50% no-repeat'
@@ -117,7 +117,7 @@ $.fn.themeswitcher = function(settings){
 	//pane css
 	switcherpane.css({
 		position: 'absolute',
-		float: 'left',
+		'float': 'left',
 		fontFamily: 'Trebuchet MS, Verdana, sans-serif',
 		fontSize: '12px',
 		background: '#000',
@@ -162,22 +162,22 @@ $.fn.themeswitcher = function(settings){
 		border: '1px solid #111',
 		'-moz-border-radius': '4px',
 		clear: 'left',
-		float: 'left'
+		'float': 'left'
 	}).end()
 	.find('a').css({
 		color: '#aaa',
 		textDecoration: 'none',
-		float: 'left',
+		'float': 'left',
 		width: '100%',
 		outline: '0'
 	}).end()
 	.find('img').css({
-		float: 'left',
+		'float': 'left',
 		border: '1px solid #333',
 		margin: '0 2px'
 	}).end()
 	.find('.themeName').css({
-		float: 'left',
+		'float': 'left',
 		margin: '3px 0'
 	}).end();
 
@@ -192,58 +192,4 @@ $.fn.themeswitcher = function(settings){
 	}
 
 	return this;
-};
-
-
-
-
-/**
- * Cookie plugin
- *
- * Copyright (c) 2006 Klaus Hartl (stilbuero.de)
- * Dual licensed under the MIT and GPL licenses:
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl.html
- *
- */
-jQuery.cookie = function(name, value, options) {
-    if (typeof value != 'undefined') { // name and value given, set cookie
-        options = options || {};
-        if (value === null) {
-            value = '';
-            options.expires = -1;
-        }
-        var expires = '';
-        if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
-            var date;
-            if (typeof options.expires == 'number') {
-                date = new Date();
-                date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
-            } else {
-                date = options.expires;
-            }
-            expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
-        }
-        // CAUTION: Needed to parenthesize options.path and options.domain
-        // in the following expressions, otherwise they evaluate to undefined
-        // in the packed version for some reason...
-        var path = options.path ? '; path=' + (options.path) : '';
-        var domain = options.domain ? '; domain=' + (options.domain) : '';
-        var secure = options.secure ? '; secure' : '';
-        document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
-    } else { // only name given, get cookie
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
 };
