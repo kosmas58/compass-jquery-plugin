6403599445fedf30f09fba54db84bed910c0f900
kosmas58 <kosmas.schuetz@gmx.com>
2010-03-31 16:01:21 +0200

    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- src/ribbon/js/jquery.ribbon.js
+++ src/ribbon/js/jquery.ribbon.js
@@ -64,16 +64,15 @@ Feel free to use this script as long as you don't remove this comment.
             	$('.ribbon-list div ul:visible').each(function() {
             		var outsideX = e.pageX < $('.ribbon-list div ul:visible').parent().offset().left || e.pageX > $('.ribbon-list div ul:visible').parent().offset().left + $('.ribbon-list div ul:visible').parent().width();
             		var outsideY = e.pageY < $('.ribbon-list div ul:visible').parent().offset().top || e.pageY > $('.ribbon-list div ul:visible').parent().offset().top + $('.ribbon-list div ul:visible').parent().height();
-            	});
-				
-                if (outsideX || outsideY) {
-                    $('.ribbon-list div ul:visible').each(function() {
-                        $(this).fadeOut('fast');
-                    });
-                    $('.ribbon-list div').css('background-image', '');
-                }
-            });
-
+	                if (outsideX || outsideY) {
+	                    $('.ribbon-list div ul:visible').each(function() {
+	                        $(this).fadeOut('fast');
+	                    });
+	                    $('.ribbon-list div').css('background-image', '');
+	                }
+               	});
+           	});
+			
             $('.orb li li a').mouseover(function() { ShowOrbChildren(this); });
 
             $('.menu li > a').dblclick(function() {
