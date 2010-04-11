8e6b78bc16baabf1acdd3a7a9ecf5859c7dbe42b
kosmas58 <kosmas.schuetz@gmx.com>
2010-04-11 16:52:13 +0200

    Typo
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- templates/secret_sauce/secret_sauce.js
+++ templates/secret_sauce/secret_sauce.js
@@ -4,9 +4,9 @@ jQuery.fn.addSauceFromForm = function(data) {
   var formData = $(data).find(".ui-dialog-content").children();
   var formToken = $(data).find("input[name='authenticity_token']");
   var formMethod = $(data).find("input[name='_method']");
-	$(data).children().remove();
+  $(data).children().remove();
   $(data).append(formData);
   $(data).append(formToken);
   $(data).append(formMethod);
-	$(this).append(data);
+  $(this).append(data);
 };
