61958e04fef50bd9d39fc52532b244a33aa3460c
kosmas58 <kosmas.schuetz@gmx.com>
2010-04-27 14:42:16 +0200

    Added a change from dsisnero
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- lib/handle_js_files.rb
+++ lib/handle_js_files.rb
@@ -5,10 +5,11 @@ YUI_JS_COMPRESSOR = File.join(GEM_ROOT, 'lib', 'yuicompressor-2.4.2.jar')
 def compress_js(scripts, compressor)
   min_js = ''
   if (compressor.downcase == "google")
-    IO.popen("java -jar #{GOOGLE_JS_COMPRESSOR} --charset utf8", 'r+') { |f| f.print(scripts); f.close_write; min_js = f.read }
+    cmd = %Q/java -jar "#{GOOGLE_JS_COMPRESSOR}" --charset utf8/ 
   else
-    IO.popen("java -jar #{YUI_JS_COMPRESSOR} --type js --charset utf8", 'r+') { |f| f.print(scripts); f.close_write; min_js = f.read }
+    cmd = %Q/java -jar "#{YUI_JS_COMPRESSOR}" --type js --charset utf8/
   end
+  IO.popen(cmd, 'r+') { |f| f.print(scripts); f.close_write; min_js = f.read } 
   min_js
 end
 
