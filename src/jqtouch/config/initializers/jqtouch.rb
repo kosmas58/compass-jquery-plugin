386f0c6d9072ab90a73b6345c61b65875810253c
kosmas58 <kosmas.schuetz@gmx.com>
2010-06-12 09:38:46 +0200

    Bugfix
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- src/jqtouch/config/initializers/jqtouch.rb
+++ src/jqtouch/config/initializers/jqtouch.rb
@@ -1,6 +1,6 @@
 require 'jquery/jqtouch'
 
-ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :jqt        => ['compiled/jqtouch/jqtouchcss']
+ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :jqt        => ['compiled/jqtouch/jqtouch.css']
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :jqt_apple  => ['compiled/jqtouch/jqt.apple.css']
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :jqt_jqt    => ['compiled/jqtouch/jqt.jqt.css']
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :jqt_iphone => ['compiled/jqtouch/iphone-emulator.css']
