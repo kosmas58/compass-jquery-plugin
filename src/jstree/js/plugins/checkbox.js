0e40a2b24601c4146816153fabb6c437209d42e0
kosmas58 <kosmas.schuetz@gmx.com>
2010-08-31 18:58:11 +0200

    Working on jsTree
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- src/jstree/js/plugins/checkbox.js
+++ src/jstree/js/plugins/checkbox.js
@@ -107,7 +107,7 @@
 
 			show_checkboxes : function () { this.get_container().children("ul").removeClass("jstree-no-checkboxes"); },
 			hide_checkboxes : function () { this.get_container().children("ul").addClass("jstree-no-checkboxes"); },
-
+	
 			_repair_state : function (obj) {
 				obj = this._get_node(obj);
 				if(!obj.length) { return; }
