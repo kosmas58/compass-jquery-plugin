619175eef272829f99492ad112a28a309eb27a4b
kosmas58 <kosmas.schuetz@gmx.com>
2010-10-30 21:26:02 +0200

    Minor fix
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- src/mobile/config/initializers/mobile.rb
+++ src/mobile/config/initializers/mobile.rb
@@ -1,3 +1,3 @@
 require 'jquery/mobile'
 
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :mobile => ['jquery-1.4.3.min', 'jquery.mobile-1.0a1.min']
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :mobile => ['jquery-1.4.3.min', 'jquery.mobile.min']
