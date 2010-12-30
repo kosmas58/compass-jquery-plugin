cec655eb0307aa081b6d4815117d8323518d8f7a
kosmas58 <kosmas.schuetz@gmx.com>
2010-12-30 10:02:09 +0100

    Working on versioning
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- lib/jquery.rb
+++ lib/jquery.rb
@@ -7,11 +7,11 @@ module Compass
     module Plugin
       version = YAML.load_file(File.dirname(__FILE__) + '/../VERSION.yml')
       if version[:commit]
-        VERSION = "#{version[:major]}.#{version[:minor]}.#{version[:patch]}.#{version[:state]}.#{version[:commit]}"
+        VERSION = "#{version[:major]}.#{version[:minor]}.#{version[:tiny]}.#{version[:state]}.#{version[:commit]}"
       elsif version[:state]
-        VERSION = "#{version[:major]}.#{version[:minor]}.#{version[:patch]}.#{version[:state]}"
+        VERSION = "#{version[:major]}.#{version[:minor]}.#{version[:tiny]}.#{version[:state]}"
       else
-        VERSION = "#{version[:major]}.#{version[:minor]}.#{version[:patch]}"
+        VERSION = "#{version[:major]}.#{version[:minor]}.#{version[:tiny]}"
       end
     end
   end
