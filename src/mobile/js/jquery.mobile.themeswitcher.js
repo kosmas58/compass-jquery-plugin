726fe116045a9d0f7cc6abe1e6455b0b662fcf0a
kosmas58 <kosmas.schuetz@gmx.com>
2011-01-27 20:18:11 +0100

    Updated jquery.mobile
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- src/mobile/js/jquery.mobile.themeswitcher.js
+++ src/mobile/js/jquery.mobile.themeswitcher.js
@@ -1,38 +1,38 @@
 //quick & dirty theme switcher, written to potentially work as a bookmarklet
 (function($){
-  $.themeswitcher = function(){
-  if( $('[data-url=themeswitcher]').length ){ return; }
-  var 
-    themesDir = '/stylesheets/compiled/jquery/mobile/',
-    //themesDir = 'http://jquerymobile.com/test/themes/',themesDir = 'http://jquerymobile.com/test/themes/',
-    themes = ['default','valencia'],
-    currentPage = $.mobile.activePage,
-    menuPage = $(
-      '<div data-url="themeswitcher" data-role=\'dialog\' data-theme=\'a\'>' +
-        '<div data-role=\'header\' data-theme=\'b\'>' +
-          '<div class=\'ui-title\'>Switch Theme:</div>'+
-        '</div>'+
-        '<div data-role=\'content\' data-theme=\'c\'><ul data-role=\'listview\' data-inset=\'true\'></ul></div>'+
-      '</div>' )
-    .appendTo( $.mobile.pageContainer ),
-    menu = menuPage.find('ul');	
+	$.themeswitcher = function(){
+		if( $('[data-url=themeswitcher]').length ){ return; }
+		var
+		  //themesDir = 'http://jquerymobile.com/test/themes/',
+		  themesDir = '/stylesheets/compiled/jquery/mobile/',
+			themes = ['default','valencia'],
+			currentPage = $.mobile.activePage,
+			menuPage = $( '<div data-url="themeswitcher" data-role=\'dialog\' data-theme=\'a\'>' +
+						'<div data-role=\'header\' data-theme=\'b\'>' +
+							'<div class=\'ui-title\'>Switch Theme:</div>'+
+						'</div>'+
+						'<div data-role=\'content\' data-theme=\'c\'><ul data-role=\'listview\' data-inset=\'true\'></ul></div>'+
+					'</div>' )
+					.appendTo( $.mobile.pageContainer ),
+			menu = menuPage.find('ul');	
+		
+		//menu items	
+		$.each(themes, function( i ){
+			$('<li><a href="#" data-rel="back">' + themes[ i ].charAt(0).toUpperCase() + themes[ i ].substr(1) + '</a></li>')
+				.click(function(){
+					addTheme( themes[i] );
+					return false;
+				})
+				.appendTo(menu);
+		});	
+		
+		//remover, adder
+		function addTheme(theme){
+			$('head').append( '<link rel=\'stylesheet\' href=\''+ themesDir + theme +'/\' />' );
+		}
 
-    //menu items	
-    $.each(themes, function( i ){
-      $('<li><a href=\'#\'>' + themes[ i ].charAt(0).toUpperCase() + themes[ i ].substr(1) + '</a></li>')
-      .click(function(){
-        addTheme( themes[i] );
-        menuPage.dialog('close');
-      })
-      .appendTo(menu);
-    });	
-    
-    //remover, adder
-    function addTheme(theme){
-      $('head').append( '<link rel=\'stylesheet\' href=\''+ themesDir + theme +'.css\' />' );
-    }
-    
-    //create page, listview
-    menuPage.page();
-  };
+		//create page, listview
+		menuPage.page();
+
+	};	
 })(jQuery);
