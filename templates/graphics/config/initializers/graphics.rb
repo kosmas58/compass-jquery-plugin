4cfceb131f2796c0fa9cb4988a87b5f343f37b5a
kosmas58 <kosmas.schuetz@gmx.com>
2011-02-20 22:20:54 +0100

    Rebuild gem
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- templates/graphics/config/initializers/graphics.rb
+++
@@ -1,5 +0,0 @@
-require 'jquery/graphics'
-
-ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :ganttView => ['compiled/jquery/gantt_view.css']
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :ganttView => ['jquery.ganttView.min']
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :sparklines => ['jquery.sparkline.min']
--- templates/ical/config/initializers/ical.rb
+++
@@ -1,7 +0,0 @@
-require 'jquery/ical'
-require 'jquery/ical/helpers/ui_event_helper'
-
-ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :ical => ['compiled/jquery/ical.css']
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :ical => ['jquery.ical.min']
-
-ActionView::Base.send :include, Ical::Helpers::UiEventHelper
