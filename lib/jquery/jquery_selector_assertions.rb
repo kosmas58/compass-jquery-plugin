99dfc47e6b9254c72d41c546a2a89712d79ecdff
kosmas58 <kosmas.schuetz@gmx.com>
2011-03-05 20:51:47 +0100

    Formatting code caused an error.
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- lib/jquery/jquery_selector_assertions.rb
+++ lib/jquery/jquery_selector_assertions.rb
@@ -30,47 +30,11 @@ module ActionController
             :insert_bottom => "\(jQuery|$\)\\(#{RJS_ANY_ID}\\)\\.append\\(#{RJS_PATTERN_HTML}\\)",
             :effect => "\(jQuery|$\)\\(#{RJS_ANY_ID}\\)\\.effect\\(",
             :highlight => "\(jQuery|$\)\\(#{RJS_ANY_ID}\\)\\.effect\\('highlight'"
-
-        =
-            begin
-              TODO :
-
-                  I 've never used the chained_* so I don' t know if they work.
-
-                                                                              I couldn 't seem to get assert_select_rjs to actually match the single quoted ids
-            which are created by some of the effects like ...
-             ... jQuery(' #item_1559').effect('highlight',{},1000);
-              so I modified jrails/lib/jrails.rb line 337
-              ... javascript = "#{JQUERY_VAR}('#{jquery_id(element_id)}').#{mode || 'effect'}('#{name}'"
-              to
-              ... javascript = "#{JQUERY_VAR}(\"#{jquery_id(element_id)}\").#{mode || 'effect'}('#{name}'"
-              so it writes double quotes like most of the others.This change should probably be
-              done to the others, but as I don 't use them so haven' t tested them.
-
-                                                                                  My other option seemed to require modifying rails ' selector_assertions.rb line 427
-             ... id ? statement.gsub(RJS_ANY_ID, "\"#{id}\"") : statement
-            which forces the expectation that the id is double quoted.  If I changed it to
-             ... statement.gsub(RJS_ANY_ID, "[\"']{1} #{id}[\"']{1}")
-              I believe that it would work as the logic seemed to work in some testing.
-                                                                                   I have not actually tried to modify rails, as this file doesn 't seem to
-            actually be in the git repository.
-
-
-            jrails now uses a nonconflict option so $ is jQuery.  I put both in the pattern in case it gets changed.
-
-                          :insert_after => "",
-                          :insert_before => "",
-            =end
-
         }
 
         [:remove, :show, :hide, :toggle, :reset].each do |action|
           RJS_STATEMENTS[action] = "\(jQuery|$\)\\(#{RJS_ANY_ID}\\)\\.#{action}\\(\\)"
         end
-
-        # TODO:
-        #RJS_STATEMENTS[:insert_html] = "Element.insert\\(#{RJS_ANY_ID}, \\{ (#{RJS_INSERTIONS.join('|')}):
-        #{RJS_PATTERN_HTML} \\}\\)"
 
         RJS_STATEMENTS[:any] = Regexp.new("(#{RJS_STATEMENTS.values.join('|')})")
         RJS_PATTERN_UNICODE_ESCAPED_CHAR = /\\u([0-9a-zA-Z]{4})/
