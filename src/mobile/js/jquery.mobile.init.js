582bd59603e9e447f14f37f676eaa9ac3cf08ceb
kosmas58 <kosmas.schuetz@gmx.com>
2011-03-26 12:36:10 +0100

    Updated jquery.mobile
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- src/mobile/js/jquery.mobile.init.js
+++ src/mobile/js/jquery.mobile.init.js
@@ -7,103 +7,103 @@
  * http://jquery.org/license
  */
 
-(function( $, window, undefined ) {
-	var	$html = $( "html" ),
-			$head = $( "head" ),
-			$window = $( window );
+(function($, window, undefined) {
+    var $html = $("html"),
+            $head = $("head"),
+            $window = $(window);
 
- 	//trigger mobileinit event - useful hook for configuring $.mobile settings before they're used
-	$( window.document ).trigger( "mobileinit" );
+    //trigger mobileinit event - useful hook for configuring $.mobile settings before they're used
+    $(window.document).trigger("mobileinit");
 
-	//support conditions
-	//if device support condition(s) aren't met, leave things as they are -> a basic, usable experience,
-	//otherwise, proceed with the enhancements
-	if ( !$.mobile.gradeA() ) {
-		return;
-	}
+    //support conditions
+    //if device support condition(s) aren't met, leave things as they are -> a basic, usable experience,
+    //otherwise, proceed with the enhancements
+    if (!$.mobile.gradeA()) {
+        return;
+    }
 
-	//add mobile, initial load "rendering" classes to docEl
-	$html.addClass( "ui-mobile ui-mobile-rendering" );
+    //add mobile, initial load "rendering" classes to docEl
+    $html.addClass("ui-mobile ui-mobile-rendering");
 
-	//define & prepend meta viewport tag, if content is defined
-	//NOTE: this is now deprecated. We recommend placing the meta viewport element in
-	//the markup from the start.
-	$.mobile.metaViewportContent && !$head.find( "meta[name='viewport']" ).length ? $( "<meta>", { name: "viewport", content: $.mobile.metaViewportContent}).prependTo( $head ) : undefined;
+    //define & prepend meta viewport tag, if content is defined
+    //NOTE: this is now deprecated. We recommend placing the meta viewport element in
+    //the markup from the start.
+    $.mobile.metaViewportContent && !$head.find("meta[name='viewport']").length ? $("<meta>", { name: "viewport", content: $.mobile.metaViewportContent}).prependTo($head) : undefined;
 
-	//loading div which appears during Ajax requests
-	//will not appear if $.mobile.loadingMessage is false
-	var $loader = $.mobile.loadingMessage ?		$( "<div class='ui-loader ui-body-a ui-corner-all'>" + "<span class='ui-icon ui-icon-loading spin'></span>" + "<h1>" + $.mobile.loadingMessage + "</h1>" + "</div>" )	: undefined;
+    //loading div which appears during Ajax requests
+    //will not appear if $.mobile.loadingMessage is false
+    var $loader = $.mobile.loadingMessage ? $("<div class='ui-loader ui-body-a ui-corner-all'>" + "<span class='ui-icon ui-icon-loading spin'></span>" + "<h1>" + $.mobile.loadingMessage + "</h1>" + "</div>") : undefined;
 
-	if(typeof $loader === "undefined"){
-		alert($.mobile.loadingMessage);
-	}
+    if (typeof $loader === "undefined") {
+        alert($.mobile.loadingMessage);
+    }
 
-	$.extend($.mobile, {
-		// turn on/off page loading message.
-		pageLoading: function ( done ) {
-			if ( done ) {
-				$html.removeClass( "ui-loading" );
-			} else {
-				if( $.mobile.loadingMessage ){
-					var activeBtn = $( "." + $.mobile.activeBtnClass ).first();
+    $.extend($.mobile, {
+        // turn on/off page loading message.
+        pageLoading: function (done) {
+            if (done) {
+                $html.removeClass("ui-loading");
+            } else {
+                if ($.mobile.loadingMessage) {
+                    var activeBtn = $("." + $.mobile.activeBtnClass).first();
 
 
-					if(typeof $loader === "undefined"){
-						 alert($.mobile.loadingMessage);
-					}
+                    if (typeof $loader === "undefined") {
+                        alert($.mobile.loadingMessage);
+                    }
 
-					$loader
-						.appendTo( $.mobile.pageContainer )
-						//position at y center (if scrollTop supported), above the activeBtn (if defined), or just 100px from top
-						.css( {
-							top: $.support.scrollTop && $(window).scrollTop() + $(window).height() / 2 ||
-							activeBtn.length && activeBtn.offset().top || 100
-						} );
-				}
+                    $loader
+                            .appendTo($.mobile.pageContainer)
+                        //position at y center (if scrollTop supported), above the activeBtn (if defined), or just 100px from top
+                            .css({
+                                     top: $.support.scrollTop && $(window).scrollTop() + $(window).height() / 2 ||
+                                             activeBtn.length && activeBtn.offset().top || 100
+                                 });
+                }
 
-				$html.addClass( "ui-loading" );
-			}
-		},
+                $html.addClass("ui-loading");
+            }
+        },
 
-		// find and enhance the pages in the dom and transition to the first page.
-		initializePage: function(){
-			//find present pages
-			var $pages = $( ":jqmData(role='page')" );
+        // find and enhance the pages in the dom and transition to the first page.
+        initializePage: function() {
+            //find present pages
+            var $pages = $(":jqmData(role='page')");
 
-			//add dialogs, set data-url attrs
-			$pages.add( ":jqmData(role='dialog')" ).each(function(){
-				var $this = $(this);
+            //add dialogs, set data-url attrs
+            $pages.add(":jqmData(role='dialog')").each(function() {
+                var $this = $(this);
 
-				// unless the data url is already set set it to the id
-				if( !$this.jqmData('url') ){
-					$this.attr( "data-" + $.mobile.ns + "url", $this.attr( "id" ) );
-				}
-			});
+                // unless the data url is already set set it to the id
+                if (!$this.jqmData('url')) {
+                    $this.attr("data-" + $.mobile.ns + "url", $this.attr("id"));
+                }
+            });
 
-			//define first page in dom case one backs out to the directory root (not always the first page visited, but defined as fallback)
-			$.mobile.firstPage = $pages.first();
+            //define first page in dom case one backs out to the directory root (not always the first page visited, but defined as fallback)
+            $.mobile.firstPage = $pages.first();
 
-			//define page container
-			$.mobile.pageContainer = $pages.first().parent().addClass( "ui-mobile-viewport" );
+            //define page container
+            $.mobile.pageContainer = $pages.first().parent().addClass("ui-mobile-viewport");
 
-			//cue page loading message
-			$.mobile.pageLoading();
+            //cue page loading message
+            $.mobile.pageLoading();
 
-			// if hashchange listening is disabled or there's no hash deeplink, change to the first page in the DOM
-			if( !$.mobile.hashListeningEnabled || !$.mobile.path.stripHash( location.hash ) ){
-				$.mobile.changePage( $.mobile.firstPage, false, true, false, true );
-			}
-			// otherwise, trigger a hashchange to load a deeplink
-			else {
-				$window.trigger( "hashchange", [ true ] );
-			}
-		}
-	});
+            // if hashchange listening is disabled or there's no hash deeplink, change to the first page in the DOM
+            if (!$.mobile.hashListeningEnabled || !$.mobile.path.stripHash(location.hash)) {
+                $.mobile.changePage($.mobile.firstPage, false, true, false, true);
+            }
+            // otherwise, trigger a hashchange to load a deeplink
+            else {
+                $window.trigger("hashchange", [ true ]);
+            }
+        }
+    });
 
-	//dom-ready inits
-	$( $.mobile.initializePage );
+    //dom-ready inits
+    $($.mobile.initializePage);
 
-	//window load event
-	//hide iOS browser chrome on load
-	$window.load( $.mobile.silentScroll );
-})( jQuery, this );
+    //window load event
+    //hide iOS browser chrome on load
+    $window.load($.mobile.silentScroll);
+})(jQuery, this);
