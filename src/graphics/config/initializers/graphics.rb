0d776ed498abe61ef538c11701e976784907b8c3
kosmas58 <kosmas.schuetz@gmx.com>
2011-04-03 17:12:20 +0200

    Added highcharts
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- src/graphics/config/initializers/graphics.rb
+++ src/graphics/config/initializers/graphics.rb
@@ -3,3 +3,8 @@ require 'jquery/graphics'
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :ganttView => ['compiled/jquery/ganttView.css']
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :ganttView => ['jquery.ganttView.min']
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :sparklines => ['jquery.sparkline.min']
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :highcharts => ['jquery.highcharts.min']
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :highcharts_blue => ['highcharts/dark-blue.min']
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :highcharts_gray => ['highcharts/gray.min']
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :highcharts_green => ['highcharts/dark-green.min']
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :highcharts_grid => ['highcharts/grid.min']
