a556eee915ab6e229668c39cc998cf87726d3442
kosmas58 <kosmas.schuetz@gmx.com>
2011-04-07 17:31:56 +0200

    Added Galaxy 7 Tab
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- src/emulators/config/initializers/emulators.rb
+++ src/emulators/config/initializers/emulators.rb
@@ -2,6 +2,8 @@ require 'jquery/emulators'
 
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :blackberry_landscape => ['compiled/emulators/blackberry.landscape.css']
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :blackberry_portrait => ['compiled/emulators/blackberry.portrait.css']
+ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :galaxy7_landscape => ['compiled/emulators/galaxy.tab.7.landscape.css']
+ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :galaxy7_portrait => ['compiled/emulators/galaxy.tab.7.portrait.css']
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :ipad_landscape => ['compiled/emulators/ipad.landscape.css']
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :ipad_portrait => ['compiled/emulators/ipad.portrait.css']
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :iphone_landscape => ['compiled/emulators/iphone.landscape.css']
