8590f50d26c7d327e74912fcf6be9b24e8ac3ad0
kosmas58 <kosmas.schuetz@gmx.com>
2011-04-13 22:07:00 +0200

    Updated jQuery UI
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- src/jquery/jquery.ui/js/jquery.effects.highlight.js
+++ src/jquery/jquery.ui/js/jquery.effects.highlight.js
@@ -1,5 +1,5 @@
 /*
- * jQuery UI Effects Highlight 1.8.11
+ * jQuery UI Effects Highlight 1.8.12
  *
  * Copyright 2011, AUTHORS.txt (http://jqueryui.com/about)
  * Dual licensed under the MIT or GPL Version 2 licenses.
@@ -10,41 +10,41 @@
  * Depends:
  *	jquery.effects.core.js
  */
-(function($, undefined) {
+(function( $, undefined ) {
 
-    $.effects.highlight = function(o) {
-        return this.queue(function() {
-            var elem = $(this),
-                    props = ['backgroundImage', 'backgroundColor', 'opacity'],
-                    mode = $.effects.setMode(elem, o.options.mode || 'show'),
-                    animation = {
-                        backgroundColor: elem.css('backgroundColor')
-                    };
+$.effects.highlight = function(o) {
+	return this.queue(function() {
+		var elem = $(this),
+			props = ['backgroundImage', 'backgroundColor', 'opacity'],
+			mode = $.effects.setMode(elem, o.options.mode || 'show'),
+			animation = {
+				backgroundColor: elem.css('backgroundColor')
+			};
 
-            if (mode == 'hide') {
-                animation.opacity = 0;
-            }
+		if (mode == 'hide') {
+			animation.opacity = 0;
+		}
 
-            $.effects.save(elem, props);
-            elem
-                    .show()
-                    .css({
-                             backgroundImage: 'none',
-                             backgroundColor: o.options.color || '#ffff99'
-                         })
-                    .animate(animation, {
-                                            queue: false,
-                                            duration: o.duration,
-                                            easing: o.options.easing,
-                                            complete: function() {
-                                                (mode == 'hide' && elem.hide());
-                                                $.effects.restore(elem, props);
-                                                (mode == 'show' && !$.support.opacity && this.style.removeAttribute('filter'));
-                                                (o.callback && o.callback.apply(this, arguments));
-                                                elem.dequeue();
-                                            }
-                                        });
-        });
-    };
+		$.effects.save(elem, props);
+		elem
+			.show()
+			.css({
+				backgroundImage: 'none',
+				backgroundColor: o.options.color || '#ffff99'
+			})
+			.animate(animation, {
+				queue: false,
+				duration: o.duration,
+				easing: o.options.easing,
+				complete: function() {
+					(mode == 'hide' && elem.hide());
+					$.effects.restore(elem, props);
+					(mode == 'show' && !$.support.opacity && this.style.removeAttribute('filter'));
+					(o.callback && o.callback.apply(this, arguments));
+					elem.dequeue();
+				}
+			});
+	});
+};
 
 })(jQuery);
--- src/jquery/jquery.ui/js/jquery.effects.pulsate.js
+++ src/jquery/jquery.ui/js/jquery.effects.pulsate.js
@@ -1,5 +1,5 @@
 /*
- * jQuery UI Effects Pulsate 1.8.11
+ * jQuery UI Effects Pulsate 1.8.12
  *
  * Copyright 2011, AUTHORS.txt (http://jqueryui.com/about)
  * Dual licensed under the MIT or GPL Version 2 licenses.
@@ -10,44 +10,42 @@
  * Depends:
  *	jquery.effects.core.js
  */
-(function($, undefined) {
+(function( $, undefined ) {
 
-    $.effects.pulsate = function(o) {
-        return this.queue(function() {
-            var elem = $(this),
-                    mode = $.effects.setMode(elem, o.options.mode || 'show');
-            times = ((o.options.times || 5) * 2) - 1;
-            duration = o.duration ? o.duration / 2 : $.fx.speeds._default / 2,
-                    isVisible = elem.is(':visible'),
-                    animateTo = 0;
+$.effects.pulsate = function(o) {
+	return this.queue(function() {
+		var elem = $(this),
+			mode = $.effects.setMode(elem, o.options.mode || 'show');
+			times = ((o.options.times || 5) * 2) - 1;
+			duration = o.duration ? o.duration / 2 : $.fx.speeds._default / 2,
+			isVisible = elem.is(':visible'),
+			animateTo = 0;
 
-            if (!isVisible) {
-                elem.css('opacity', 0).show();
-                animateTo = 1;
-            }
+		if (!isVisible) {
+			elem.css('opacity', 0).show();
+			animateTo = 1;
+		}
 
-            if ((mode == 'hide' && isVisible) || (mode == 'show' && !isVisible)) {
-                times--;
-            }
+		if ((mode == 'hide' && isVisible) || (mode == 'show' && !isVisible)) {
+			times--;
+		}
 
-            for (var i = 0; i < times; i++) {
-                elem.animate({ opacity: animateTo }, duration, o.options.easing);
-                animateTo = (animateTo + 1) % 2;
-            }
+		for (var i = 0; i < times; i++) {
+			elem.animate({ opacity: animateTo }, duration, o.options.easing);
+			animateTo = (animateTo + 1) % 2;
+		}
 
-            elem.animate({ opacity: animateTo }, duration, o.options.easing, function() {
-                if (animateTo == 0) {
-                    elem.hide();
-                }
-                (o.callback && o.callback.apply(this, arguments));
-            });
+		elem.animate({ opacity: animateTo }, duration, o.options.easing, function() {
+			if (animateTo == 0) {
+				elem.hide();
+			}
+			(o.callback && o.callback.apply(this, arguments));
+		});
 
-            elem
-                    .queue('fx', function() {
-                elem.dequeue();
-            })
-                    .dequeue();
-        });
-    };
+		elem
+			.queue('fx', function() { elem.dequeue(); })
+			.dequeue();
+	});
+};
 
 })(jQuery);
