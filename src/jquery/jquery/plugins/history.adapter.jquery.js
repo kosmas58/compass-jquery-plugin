88cae2e11903ffee2a1f09a3df526f98ed0e8f8b
kosmas58 <kosmas.schuetz@gmx.com>
2011-04-23 22:06:15 +0200

    Reformatted code
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- src/jquery/jquery/plugins/history.adapter.jquery.js
+++ src/jquery/jquery/plugins/history.adapter.jquery.js
@@ -7,46 +7,46 @@
 
 // Closure
 (function(window, undefined) {
-    // Localise Globals
-    var
-            History = window.History = window.History || {},
-            jQuery = window.jQuery;
+  // Localise Globals
+  var
+          History = window.History = window.History || {},
+          jQuery = window.jQuery;
 
-    // Check Existence
-    if (typeof History.Adapter !== 'undefined') {
-        throw new Error('History.js Adapter has already been loaded...');
+  // Check Existence
+  if (typeof History.Adapter !== 'undefined') {
+    throw new Error('History.js Adapter has already been loaded...');
+  }
+
+  // Add the Adapter
+  History.Adapter = {
+    // History.Adapter.bind(el,event,callback)
+    // @param {Element|Selector} el
+    // @param {String} event - custom and standard events
+    // @param {Function} callback
+    // @return
+    bind: function(el, event, callback) {
+      jQuery(el).bind(event, callback);
+    },
+
+    // History.Adapter.trigger(el,event)
+    // @param {Element|Selector} el
+    // @param {String} event - custom and standard events
+    // @return
+    trigger: function(el, event) {
+      jQuery(el).trigger(event);
+    },
+
+    // History.Adapter.trigger(el,event,data)
+    // @param {Function} callback
+    // @return
+    onDomLoad: function(callback) {
+      jQuery(callback);
     }
+  };
 
-    // Add the Adapter
-    History.Adapter = {
-        // History.Adapter.bind(el,event,callback)
-        // @param {Element|Selector} el
-        // @param {String} event - custom and standard events
-        // @param {Function} callback
-        // @return
-        bind: function(el, event, callback) {
-            jQuery(el).bind(event, callback);
-        },
-
-        // History.Adapter.trigger(el,event)
-        // @param {Element|Selector} el
-        // @param {String} event - custom and standard events
-        // @return
-        trigger: function(el, event) {
-            jQuery(el).trigger(event);
-        },
-
-        // History.Adapter.trigger(el,event,data)
-        // @param {Function} callback
-        // @return
-        onDomLoad: function(callback) {
-            jQuery(callback);
-        }
-    };
-
-    // Try and Initialise History
-    if (typeof History.init !== 'undefined') {
-        History.init();
-    }
+  // Try and Initialise History
+  if (typeof History.init !== 'undefined') {
+    History.init();
+  }
 
 })(window);
