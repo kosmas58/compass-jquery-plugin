8d8a2c4d9dc108555c2f321dd778ee0c5764d143
kosmas58 <kosmas.schuetz@gmx.com>
2011-04-23 21:55:09 +0200

    Reformatted code
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- src/tools/js/scrollable.navigator.js
+++ src/tools/js/scrollable.navigator.js
@@ -11,144 +11,144 @@
  */
 (function($) {
 
-    var t = $.tools.scrollable;
+  var t = $.tools.scrollable;
 
-    t.navigator = {
+  t.navigator = {
 
-        conf: {
-            navi: '.navi',
-            naviItem: null,
-            activeClass: 'active',
-            indexed: false,
-            idPrefix: null,
+    conf: {
+      navi: '.navi',
+      naviItem: null,
+      activeClass: 'active',
+      indexed: false,
+      idPrefix: null,
 
-            // 1.2
-            history: false
-        }
-    };
-
-    function find(root, query) {
-        var el = $(query);
-        return el.length < 2 ? el : root.parent().find(query);
+      // 1.2
+      history: false
     }
+  };
 
-    // jQuery plugin implementation
-    $.fn.navigator = function(conf) {
+  function find(root, query) {
+    var el = $(query);
+    return el.length < 2 ? el : root.parent().find(query);
+  }
 
-        // configuration
-        if (typeof conf == 'string') {
-            conf = {navi: conf};
+  // jQuery plugin implementation
+  $.fn.navigator = function(conf) {
+
+    // configuration
+    if (typeof conf == 'string') {
+      conf = {navi: conf};
+    }
+    conf = $.extend({}, t.navigator.conf, conf);
+
+    var ret;
+
+    this.each(function() {
+
+      var api = $(this).data("scrollable"),
+              navi = conf.navi.jquery ? conf.navi : find(api.getRoot(), conf.navi),
+              buttons = api.getNaviButtons(),
+              cls = conf.activeClass,
+              history = conf.history && $.fn.history;
+
+      // @deprecated stuff
+      if (api) {
+        ret = api;
+      }
+
+      api.getNaviButtons = function() {
+        return buttons.add(navi);
+      };
+
+
+      function doClick(el, i, e) {
+        api.seekTo(i);
+        if (history) {
+          if (location.hash) {
+            location.hash = el.attr("href").replace("#", "");
+          }
+        } else {
+          return e.preventDefault();
         }
-        conf = $.extend({}, t.navigator.conf, conf);
+      }
 
-        var ret;
+      function els() {
+        return navi.find(conf.naviItem || '> *');
+      }
 
-        this.each(function() {
+      function addItem(i) {
 
-            var api = $(this).data("scrollable"),
-                    navi = conf.navi.jquery ? conf.navi : find(api.getRoot(), conf.navi),
-                    buttons = api.getNaviButtons(),
-                    cls = conf.activeClass,
-                    history = conf.history && $.fn.history;
+        var item = $("<" + (conf.naviItem || 'a') + "/>").click(
+                function(e) {
+                  doClick($(this), i, e);
 
-            // @deprecated stuff
-            if (api) {
-                ret = api;
-            }
+                }).attr("href", "#" + i);
 
-            api.getNaviButtons = function() {
-                return buttons.add(navi);
-            };
+        // index number / id attribute
+        if (i === 0) {
+          item.addClass(cls);
+        }
+        if (conf.indexed) {
+          item.text(i + 1);
+        }
+        if (conf.idPrefix) {
+          item.attr("id", conf.idPrefix + i);
+        }
 
-
-            function doClick(el, i, e) {
-                api.seekTo(i);
-                if (history) {
-                    if (location.hash) {
-                        location.hash = el.attr("href").replace("#", "");
-                    }
-                } else {
-                    return e.preventDefault();
-                }
-            }
-
-            function els() {
-                return navi.find(conf.naviItem || '> *');
-            }
-
-            function addItem(i) {
-
-                var item = $("<" + (conf.naviItem || 'a') + "/>").click(
-                        function(e) {
-                            doClick($(this), i, e);
-
-                        }).attr("href", "#" + i);
-
-                // index number / id attribute
-                if (i === 0) {
-                    item.addClass(cls);
-                }
-                if (conf.indexed) {
-                    item.text(i + 1);
-                }
-                if (conf.idPrefix) {
-                    item.attr("id", conf.idPrefix + i);
-                }
-
-                return item.appendTo(navi);
-            }
+        return item.appendTo(navi);
+      }
 
 
-            // generate navigator
-            if (els().length) {
-                els().each(function(i) {
-                    $(this).click(function(e) {
-                        doClick($(this), i, e);
-                    });
-                });
-
-            } else {
-                $.each(api.getItems(), function(i) {
-                    addItem(i);
-                });
-            }
-
-            // activate correct entry
-            api.onBeforeSeek(function(e, index) {
-                setTimeout(function() {
-                    if (!e.isDefaultPrevented()) {
-                        var el = els().eq(index);
-                        if (!e.isDefaultPrevented() && el.length) {
-                            els().removeClass(cls).eq(index).addClass(cls);
-                        }
-                    }
-                }, 1);
-            });
-
-            function doHistory(evt, hash) {
-                var el = els().eq(hash.replace("#", ""));
-                if (!el.length) {
-                    el = els().filter("[href=" + hash + "]");
-                }
-                el.click();
-            }
-
-            // new item being added
-            api.onAddItem(function(e, item) {
-                item = addItem(api.getItems().index(item));
-                if (history) {
-                    item.history(doHistory);
-                }
-            });
-
-            if (history) {
-                els().history(doHistory);
-            }
-
+      // generate navigator
+      if (els().length) {
+        els().each(function(i) {
+          $(this).click(function(e) {
+            doClick($(this), i, e);
+          });
         });
 
-        return conf.api ? ret : this;
+      } else {
+        $.each(api.getItems(), function(i) {
+          addItem(i);
+        });
+      }
 
-    };
+      // activate correct entry
+      api.onBeforeSeek(function(e, index) {
+        setTimeout(function() {
+          if (!e.isDefaultPrevented()) {
+            var el = els().eq(index);
+            if (!e.isDefaultPrevented() && el.length) {
+              els().removeClass(cls).eq(index).addClass(cls);
+            }
+          }
+        }, 1);
+      });
+
+      function doHistory(evt, hash) {
+        var el = els().eq(hash.replace("#", ""));
+        if (!el.length) {
+          el = els().filter("[href=" + hash + "]");
+        }
+        el.click();
+      }
+
+      // new item being added
+      api.onAddItem(function(e, item) {
+        item = addItem(api.getItems().index(item));
+        if (history) {
+          item.history(doHistory);
+        }
+      });
+
+      if (history) {
+        els().history(doHistory);
+      }
+
+    });
+
+    return conf.api ? ret : this;
+
+  };
 
 })(jQuery);			
--- src/tools/js/tooltip.slide.js
+++ src/tools/js/tooltip.slide.js
@@ -11,72 +11,72 @@
  */
 (function($) {
 
-    // version number
-    var t = $.tools.tooltip;
+  // version number
+  var t = $.tools.tooltip;
 
-    // extend global configuragion with effect specific defaults
-    $.extend(t.conf, {
-        direction: 'up', // down, left, right
-        bounce: false,
-        slideOffset: 10,
-        slideInSpeed: 200,
-        slideOutSpeed: 200,
-        slideFade: !$.browser.msie
-    });
+  // extend global configuragion with effect specific defaults
+  $.extend(t.conf, {
+    direction: 'up', // down, left, right
+    bounce: false,
+    slideOffset: 10,
+    slideInSpeed: 200,
+    slideOutSpeed: 200,
+    slideFade: !$.browser.msie
+  });
 
-    // directions for slide effect
-    var dirs = {
-        up: ['-', 'top'],
-        down: ['+', 'top'],
-        left: ['-', 'left'],
-        right: ['+', 'left']
-    };
+  // directions for slide effect
+  var dirs = {
+    up: ['-', 'top'],
+    down: ['+', 'top'],
+    left: ['-', 'left'],
+    right: ['+', 'left']
+  };
 
-    /* default effect: "slide"  */
-    t.addEffect("slide",
+  /* default effect: "slide"  */
+  t.addEffect("slide",
 
-        // show effect
-            function(done) {
+    // show effect
+          function(done) {
 
-                // variables
-                var conf = this.getConf(),
-                        tip = this.getTip(),
-                        params = conf.slideFade ? {opacity: conf.opacity} : {},
-                        dir = dirs[conf.direction] || dirs.up;
+            // variables
+            var conf = this.getConf(),
+                    tip = this.getTip(),
+                    params = conf.slideFade ? {opacity: conf.opacity} : {},
+                    dir = dirs[conf.direction] || dirs.up;
 
-                // direction
-                params[dir[1]] = dir[0] + '=' + conf.slideOffset;
+            // direction
+            params[dir[1]] = dir[0] + '=' + conf.slideOffset;
 
-                // perform animation
-                if (conf.slideFade) {
-                    tip.css({opacity:0});
-                }
-                tip.show().animate(params, conf.slideInSpeed, done);
-            },
-
-        // hide effect
-            function(done) {
-
-                // variables
-                var conf = this.getConf(),
-                        offset = conf.slideOffset,
-                        params = conf.slideFade ? {opacity: 0} : {},
-                        dir = dirs[conf.direction] || dirs.up;
-
-                // direction
-                var sign = "" + dir[0];
-                if (conf.bounce) {
-                    sign = sign == '+' ? '-' : '+';
-                }
-                params[dir[1]] = sign + '=' + offset;
-
-                // perform animation
-                this.getTip().animate(params, conf.slideOutSpeed, function() {
-                    $(this).hide();
-                    done.call();
-                });
+            // perform animation
+            if (conf.slideFade) {
+              tip.css({opacity:0});
             }
-            );
+            tip.show().animate(params, conf.slideInSpeed, done);
+          },
+
+    // hide effect
+          function(done) {
+
+            // variables
+            var conf = this.getConf(),
+                    offset = conf.slideOffset,
+                    params = conf.slideFade ? {opacity: 0} : {},
+                    dir = dirs[conf.direction] || dirs.up;
+
+            // direction
+            var sign = "" + dir[0];
+            if (conf.bounce) {
+              sign = sign == '+' ? '-' : '+';
+            }
+            params[dir[1]] = sign + '=' + offset;
+
+            // perform animation
+            this.getTip().animate(params, conf.slideOutSpeed, function() {
+              $(this).hide();
+              done.call();
+            });
+          }
+          );
 
 })(jQuery);	
 		
