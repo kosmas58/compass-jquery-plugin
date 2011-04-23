477ecc2d8ab6eea5d49289046d8dec8a1136091e
kosmas58 <kosmas.schuetz@gmx.com>
2011-04-23 21:48:23 +0200

    Reformatted stuff
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- src/mobile/js/jquery.mobile.event.js
+++ src/mobile/js/jquery.mobile.event.js
@@ -7,244 +7,244 @@
 (function($, undefined) {
 
 // add new event shortcuts
-    $.each("touchstart touchmove touchend orientationchange tap taphold swipe swipeleft swiperight scrollstart scrollstop".split(" "), function(i, name) {
-        $.fn[ name ] = function(fn) {
-            return fn ? this.bind(name, fn) : this.trigger(name);
-        };
-        $.attrFn[ name ] = true;
-    });
+  $.each("touchstart touchmove touchend orientationchange tap taphold swipe swipeleft swiperight scrollstart scrollstop".split(" "), function(i, name) {
+    $.fn[ name ] = function(fn) {
+      return fn ? this.bind(name, fn) : this.trigger(name);
+    };
+    $.attrFn[ name ] = true;
+  });
 
-    var supportTouch = $.support.touch,
-            scrollEvent = "touchmove scroll",
-            touchStartEvent = supportTouch ? "touchstart" : "mousedown",
-            touchStopEvent = supportTouch ? "touchend" : "mouseup",
-            touchMoveEvent = supportTouch ? "touchmove" : "mousemove";
+  var supportTouch = $.support.touch,
+          scrollEvent = "touchmove scroll",
+          touchStartEvent = supportTouch ? "touchstart" : "mousedown",
+          touchStopEvent = supportTouch ? "touchend" : "mouseup",
+          touchMoveEvent = supportTouch ? "touchmove" : "mousemove";
 
-    function triggerCustomEvent(obj, eventType, event) {
-        var originalType = event.type;
-        event.type = eventType;
-        $.event.handle.call(obj, event);
-        event.type = originalType;
-    }
+  function triggerCustomEvent(obj, eventType, event) {
+    var originalType = event.type;
+    event.type = eventType;
+    $.event.handle.call(obj, event);
+    event.type = originalType;
+  }
 
 // also handles scrollstop
-    $.event.special.scrollstart = {
-        enabled: true,
+  $.event.special.scrollstart = {
+    enabled: true,
 
-        setup: function() {
-            var thisObject = this,
-                    $this = $(thisObject),
-                    scrolling,
-                    timer;
+    setup: function() {
+      var thisObject = this,
+              $this = $(thisObject),
+              scrolling,
+              timer;
 
-            function trigger(event, state) {
-                scrolling = state;
-                triggerCustomEvent(thisObject, scrolling ? "scrollstart" : "scrollstop", event);
-            }
+      function trigger(event, state) {
+        scrolling = state;
+        triggerCustomEvent(thisObject, scrolling ? "scrollstart" : "scrollstop", event);
+      }
 
-            // iPhone triggers scroll after a small delay; use touchmove instead
-            $this.bind(scrollEvent, function(event) {
-                if (!$.event.special.scrollstart.enabled) {
-                    return;
-                }
-
-                if (!scrolling) {
-                    trigger(event, true);
-                }
-
-                clearTimeout(timer);
-                timer = setTimeout(function() {
-                    trigger(event, false);
-                }, 50);
-            });
+      // iPhone triggers scroll after a small delay; use touchmove instead
+      $this.bind(scrollEvent, function(event) {
+        if (!$.event.special.scrollstart.enabled) {
+          return;
         }
-    };
+
+        if (!scrolling) {
+          trigger(event, true);
+        }
+
+        clearTimeout(timer);
+        timer = setTimeout(function() {
+          trigger(event, false);
+        }, 50);
+      });
+    }
+  };
 
 // also handles taphold
-    $.event.special.tap = {
-        setup: function() {
-            var thisObject = this,
-                    $this = $(thisObject);
+  $.event.special.tap = {
+    setup: function() {
+      var thisObject = this,
+              $this = $(thisObject);
 
-            $this
-                    .bind("vmousedown", function(event) {
-                if (event.which && event.which !== 1) {
-                    return false;
-                }
-
-                var touching = true,
-                        origTarget = event.target,
-                        origEvent = event.originalEvent,
-                        timer;
-
-                function clearTapHandlers() {
-                    touching = false;
-                    clearTimeout(timer);
-                    $this.unbind("vclick", clickHandler).unbind("vmousecancel", clearTapHandlers);
-                }
-
-                function clickHandler(event) {
-                    clearTapHandlers();
-
-                    /* ONLY trigger a 'tap' event if the start target is
-                     * the same as the stop target.
-                     */
-                    if (origTarget == event.target) {
-                        triggerCustomEvent(thisObject, "tap", event);
-                    }
-                }
-
-                $this.bind("vmousecancel", clearTapHandlers).bind("vclick", clickHandler);
-
-                timer = setTimeout(function() {
-                    if (touching) {
-                        triggerCustomEvent(thisObject, "taphold", event);
-                    }
-                }, 750);
-            });
+      $this
+              .bind("vmousedown", function(event) {
+        if (event.which && event.which !== 1) {
+          return false;
         }
-    };
+
+        var touching = true,
+                origTarget = event.target,
+                origEvent = event.originalEvent,
+                timer;
+
+        function clearTapHandlers() {
+          touching = false;
+          clearTimeout(timer);
+          $this.unbind("vclick", clickHandler).unbind("vmousecancel", clearTapHandlers);
+        }
+
+        function clickHandler(event) {
+          clearTapHandlers();
+
+          /* ONLY trigger a 'tap' event if the start target is
+           * the same as the stop target.
+           */
+          if (origTarget == event.target) {
+            triggerCustomEvent(thisObject, "tap", event);
+          }
+        }
+
+        $this.bind("vmousecancel", clearTapHandlers).bind("vclick", clickHandler);
+
+        timer = setTimeout(function() {
+          if (touching) {
+            triggerCustomEvent(thisObject, "taphold", event);
+          }
+        }, 750);
+      });
+    }
+  };
 
 // also handles swipeleft, swiperight
-    $.event.special.swipe = {
-        setup: function() {
-            var thisObject = this,
-                    $this = $(thisObject);
+  $.event.special.swipe = {
+    setup: function() {
+      var thisObject = this,
+              $this = $(thisObject);
 
-            $this
-                    .bind(touchStartEvent, function(event) {
-                var data = event.originalEvent.touches ?
-                        event.originalEvent.touches[ 0 ] :
-                        event,
-                        start = {
-                            time: (new Date).getTime(),
-                            coords: [ data.pageX, data.pageY ],
-                            origin: $(event.target)
-                        },
-                        stop;
+      $this
+              .bind(touchStartEvent, function(event) {
+        var data = event.originalEvent.touches ?
+                event.originalEvent.touches[ 0 ] :
+                event,
+                start = {
+                  time: (new Date).getTime(),
+                  coords: [ data.pageX, data.pageY ],
+                  origin: $(event.target)
+                },
+                stop;
 
-                function moveHandler(event) {
-                    if (!start) {
-                        return;
-                    }
+        function moveHandler(event) {
+          if (!start) {
+            return;
+          }
 
-                    var data = event.originalEvent.touches ?
-                            event.originalEvent.touches[ 0 ] :
-                            event;
-                    stop = {
-                        time: (new Date).getTime(),
-                        coords: [ data.pageX, data.pageY ]
-                    };
+          var data = event.originalEvent.touches ?
+                  event.originalEvent.touches[ 0 ] :
+                  event;
+          stop = {
+            time: (new Date).getTime(),
+            coords: [ data.pageX, data.pageY ]
+          };
 
-                    // prevent scrolling
-                    if (Math.abs(start.coords[0] - stop.coords[0]) > 10) {
-                        event.preventDefault();
-                    }
-                }
-
-                $this
-                        .bind(touchMoveEvent, moveHandler)
-                        .one(touchStopEvent, function(event) {
-                    $this.unbind(touchMoveEvent, moveHandler);
-                    if (start && stop) {
-                        if (stop.time - start.time < 1000 &&
-                                Math.abs(start.coords[0] - stop.coords[0]) > 30 &&
-                                Math.abs(start.coords[1] - stop.coords[1]) < 75) {
-                            start.origin
-                                    .trigger("swipe")
-
-                                    .trigger(start.coords[0] > stop.coords[0] ? "swipeleft" : "swiperight");
-                        }
-                    }
-                    start = stop = undefined;
-                });
-            });
+          // prevent scrolling
+          if (Math.abs(start.coords[0] - stop.coords[0]) > 10) {
+            event.preventDefault();
+          }
         }
+
+        $this
+                .bind(touchMoveEvent, moveHandler)
+                .one(touchStopEvent, function(event) {
+          $this.unbind(touchMoveEvent, moveHandler);
+          if (start && stop) {
+            if (stop.time - start.time < 1000 &&
+                    Math.abs(start.coords[0] - stop.coords[0]) > 30 &&
+                    Math.abs(start.coords[1] - stop.coords[1]) < 75) {
+              start.origin
+                      .trigger("swipe")
+
+                      .trigger(start.coords[0] > stop.coords[0] ? "swipeleft" : "swiperight");
+            }
+          }
+          start = stop = undefined;
+        });
+      });
+    }
+  };
+
+  (function($) {
+    // "Cowboy" Ben Alman
+
+    var win = $(window),
+            special_event,
+            get_orientation,
+            last_orientation;
+
+    $.event.special.orientationchange = special_event = {
+      setup: function() {
+        // If the event is supported natively, return false so that jQuery
+        // will bind to the event using DOM methods.
+        if ($.support.orientation) {
+          return false;
+        }
+
+        // Get the current orientation to avoid initial double-triggering.
+        last_orientation = get_orientation();
+
+        // Because the orientationchange event doesn't exist, simulate the
+        // event by testing window dimensions on resize.
+        win.bind("resize", handler);
+      },
+      teardown: function() {
+        // If the event is not supported natively, return false so that
+        // jQuery will unbind the event using DOM methods.
+        if ($.support.orientation) {
+          return false;
+        }
+
+        // Because the orientationchange event doesn't exist, unbind the
+        // resize event handler.
+        win.unbind("resize", handler);
+      },
+      add: function(handleObj) {
+        // Save a reference to the bound event handler.
+        var old_handler = handleObj.handler;
+
+        handleObj.handler = function(event) {
+          // Modify event object, adding the .orientation property.
+          event.orientation = get_orientation();
+
+          // Call the originally-bound event handler and return its result.
+          return old_handler.apply(this, arguments);
+        };
+      }
     };
 
-    (function($) {
-        // "Cowboy" Ben Alman
+    // If the event is not supported natively, this handler will be bound to
+    // the window resize event to simulate the orientationchange event.
+    function handler() {
+      // Get the current orientation.
+      var orientation = get_orientation();
 
-        var win = $(window),
-                special_event,
-                get_orientation,
-                last_orientation;
+      if (orientation !== last_orientation) {
+        // The orientation has changed, so trigger the orientationchange event.
+        last_orientation = orientation;
+        win.trigger("orientationchange");
+      }
+    }
 
-        $.event.special.orientationchange = special_event = {
-            setup: function() {
-                // If the event is supported natively, return false so that jQuery
-                // will bind to the event using DOM methods.
-                if ($.support.orientation) {
-                    return false;
-                }
+    ;
 
-                // Get the current orientation to avoid initial double-triggering.
-                last_orientation = get_orientation();
+    // Get the current page orientation. This method is exposed publicly, should it
+    // be needed, as jQuery.event.special.orientationchange.orientation()
+    special_event.orientation = get_orientation = function() {
+      var elem = document.documentElement;
+      return elem && elem.clientWidth / elem.clientHeight < 1.1 ? "portrait" : "landscape";
+    };
 
-                // Because the orientationchange event doesn't exist, simulate the
-                // event by testing window dimensions on resize.
-                win.bind("resize", handler);
-            },
-            teardown: function() {
-                // If the event is not supported natively, return false so that
-                // jQuery will unbind the event using DOM methods.
-                if ($.support.orientation) {
-                    return false;
-                }
+  })(jQuery);
 
-                // Because the orientationchange event doesn't exist, unbind the
-                // resize event handler.
-                win.unbind("resize", handler);
-            },
-            add: function(handleObj) {
-                // Save a reference to the bound event handler.
-                var old_handler = handleObj.handler;
-
-                handleObj.handler = function(event) {
-                    // Modify event object, adding the .orientation property.
-                    event.orientation = get_orientation();
-
-                    // Call the originally-bound event handler and return its result.
-                    return old_handler.apply(this, arguments);
-                };
-            }
-        };
-
-        // If the event is not supported natively, this handler will be bound to
-        // the window resize event to simulate the orientationchange event.
-        function handler() {
-            // Get the current orientation.
-            var orientation = get_orientation();
-
-            if (orientation !== last_orientation) {
-                // The orientation has changed, so trigger the orientationchange event.
-                last_orientation = orientation;
-                win.trigger("orientationchange");
-            }
-        }
-
-        ;
-
-        // Get the current page orientation. This method is exposed publicly, should it
-        // be needed, as jQuery.event.special.orientationchange.orientation()
-        special_event.orientation = get_orientation = function() {
-            var elem = document.documentElement;
-            return elem && elem.clientWidth / elem.clientHeight < 1.1 ? "portrait" : "landscape";
-        };
-
-    })(jQuery);
-
-    $.each({
-        scrollstop: "scrollstart",
-        taphold: "tap",
-        swipeleft: "swipe",
-        swiperight: "swipe"
-    }, function(event, sourceEvent) {
-        $.event.special[ event ] = {
-            setup: function() {
-                $(this).bind(sourceEvent, $.noop);
-            }
-        };
-    });
+  $.each({
+    scrollstop: "scrollstart",
+    taphold: "tap",
+    swipeleft: "swipe",
+    swiperight: "swipe"
+  }, function(event, sourceEvent) {
+    $.event.special[ event ] = {
+      setup: function() {
+        $(this).bind(sourceEvent, $.noop);
+      }
+    };
+  });
 
 })(jQuery);
--- src/mobile/js/jquery.mobile.navbar.js
+++ src/mobile/js/jquery.mobile.navbar.js
@@ -5,37 +5,37 @@
  * http://jquery.org/license
  */
 (function($, undefined) {
-    $.widget("mobile.navbar", $.mobile.widget, {
-        options: {
-            iconpos: 'top',
-            grid: null
-        },
-        _create: function() {
-            var $navbar = this.element,
-                    $navbtns = $navbar.find("a"),
-                    iconpos = $navbtns.filter(":jqmData(icon)").length ? this.options.iconpos : undefined;
+  $.widget("mobile.navbar", $.mobile.widget, {
+    options: {
+      iconpos: 'top',
+      grid: null
+    },
+    _create: function() {
+      var $navbar = this.element,
+              $navbtns = $navbar.find("a"),
+              iconpos = $navbtns.filter(":jqmData(icon)").length ? this.options.iconpos : undefined;
 
-            $navbar
-                    .addClass('ui-navbar')
-                    .attr("role", "navigation")
-                    .find("ul")
-                    .grid({grid: this.options.grid });
+      $navbar
+              .addClass('ui-navbar')
+              .attr("role", "navigation")
+              .find("ul")
+              .grid({grid: this.options.grid });
 
-            if (!iconpos) {
-                $navbar.addClass("ui-navbar-noicons");
-            }
+      if (!iconpos) {
+        $navbar.addClass("ui-navbar-noicons");
+      }
 
-            $navbtns
-                    .buttonMarkup({
-                                      corners:    false,
-                                      shadow:        false,
-                                      iconpos:    iconpos
-                                  });
+      $navbtns
+              .buttonMarkup({
+                              corners:  false,
+                              shadow:    false,
+                              iconpos:  iconpos
+                            });
 
-            $navbar.delegate("a", "vclick", function(event) {
-                $navbtns.not(".ui-state-persist").removeClass($.mobile.activeBtnClass);
-                $(this).addClass($.mobile.activeBtnClass);
-            });
-        }
-    });
+      $navbar.delegate("a", "vclick", function(event) {
+        $navbtns.not(".ui-state-persist").removeClass($.mobile.activeBtnClass);
+        $(this).addClass($.mobile.activeBtnClass);
+      });
+    }
+  });
 })(jQuery);
