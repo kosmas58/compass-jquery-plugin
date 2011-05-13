94aec7fcf77c39d95c606434351a097aac4e2fcf
kosmas58 <kosmas.schuetz@gmx.com>
2011-05-13 18:20:28 +0200

    Gone back to jQuery 1.0a4.1
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- src/mobile/js/jquery.mobile.forms.checkboxradio.js
+++ src/mobile/js/jquery.mobile.forms.checkboxradio.js
@@ -1,157 +1,144 @@
 /*
- * jQuery Mobile Framework : "checkboxradio" plugin
- * Copyright (c) jQuery Project
- * Dual licensed under the MIT or GPL Version 2 licenses.
- * http://jquery.org/license
- */
-(function($, undefined) {
-  $.widget("mobile.checkboxradio", $.mobile.widget, {
-    options: {
-      theme: null
-    },
-    _create: function() {
-      var self = this,
-              input = this.element,
-        //NOTE: Windows Phone could not find the label through a selector
-        //filter works though.
-              label = input.closest("form,fieldset,:jqmData(role='page')").find("label").filter('[for="' + input[0].id + '"]'),
-              inputtype = input.attr("type"),
-              checkedicon = "ui-icon-" + inputtype + "-on",
-              uncheckedicon = "ui-icon-" + inputtype + "-off";
+* jQuery Mobile Framework : "checkboxradio" plugin
+* Copyright (c) jQuery Project
+* Dual licensed under the MIT or GPL Version 2 licenses.
+* http://jquery.org/license
+*/
+(function($, undefined ) {
+$.widget( "mobile.checkboxradio", $.mobile.widget, {
+	options: {
+		theme: null
+	},
+	_create: function(){
+		var self = this,
+			input = this.element,
+			//NOTE: Windows Phone could not find the label through a selector
+			//filter works though.
+			label = input.closest("form,fieldset,:jqmData(role='page')").find("label").filter("[for=" + input[0].id + "]"),
+			inputtype = input.attr( "type" ),
+			checkedicon = "ui-icon-" + inputtype + "-on",
+			uncheckedicon = "ui-icon-" + inputtype + "-off";
 
-      if (inputtype != "checkbox" && inputtype != "radio") {
-        return;
-      }
+		if ( inputtype != "checkbox" && inputtype != "radio" ) { return; }
 
-      //expose for other methods
-      $.extend(this, {
-        label            : label,
-        inputtype        : inputtype,
-        checkedicon        : checkedicon,
-        uncheckedicon    : uncheckedicon
-      });
+		//expose for other methods
+		$.extend( this,{
+			label			: label,
+			inputtype		: inputtype,
+			checkedicon		: checkedicon,
+			uncheckedicon	: uncheckedicon
+		});
 
-      // If there's no selected theme...
-      if (!this.options.theme) {
-        this.options.theme = this.element.jqmData("theme");
-      }
+		// If there's no selected theme...
+		if( !this.options.theme ) {
+			this.options.theme = this.element.jqmData( "theme" );
+		}
 
-      label
-              .buttonMarkup({
-                              theme: this.options.theme,
-                              icon: this.element.parents(":jqmData(type='horizontal')").length ? undefined : uncheckedicon,
-                              shadow: false
-                            });
+		label
+			.buttonMarkup({
+				theme: this.options.theme,
+				icon: this.element.parents( ":jqmData(type='horizontal')" ).length ? undefined : uncheckedicon,
+				shadow: false
+			});
 
-      // wrap the input + label in a div
-      input
-              .add(label)
-              .wrapAll("<div class='ui-" + inputtype + "'></div>");
+		// wrap the input + label in a div
+		input
+			.add( label )
+			.wrapAll( "<div class='ui-" + inputtype +"'></div>" );
 
-      label.bind({
-        vmouseover: function() {
-          if ($(this).parent().is('.ui-disabled')) {
-            return false;
-          }
-        },
+		label.bind({
+			vmouseover: function() {
+				if( $(this).parent().is('.ui-disabled') ){ return false; }
+			},
 
-        vclick: function(event) {
-          if (input.is(":disabled")) {
-            event.preventDefault();
-            return;
-          }
+			vclick: function( event ){
+				if ( input.is( ":disabled" ) ){
+					event.preventDefault();
+					return;
+				}
 
-          self._cacheVals();
+				self._cacheVals();
+				input.attr( "checked", inputtype === "radio" && true || !input.is( ":checked" ) );
+				self._updateAll();
+				return false;
+			}
 
-          input.attr("checked", inputtype === "radio" && true || !input.is(":checked"));
+		});
 
-          // input set for common radio buttons will contain all the radio
-          // buttons, but will not for checkboxes. clearing the checked status
-          // of other radios ensures the active button state is applied properly
-          self._getInputSet().not(input).removeAttr('checked');
+		input
+			.bind({
+				vmousedown: function(){
+					this._cacheVals();
+				},
 
-          self._updateAll();
-          return false;
-        }
+				vclick: function(){
+					self._updateAll();
+				},
 
-      });
+				focus: function() {
+					label.addClass( "ui-focus" );
+				},
 
-      input
-              .bind({
-                      vmousedown: function() {
-                        this._cacheVals();
-                      },
+				blur: function() {
+					label.removeClass( "ui-focus" );
+				}
+			});
 
-                      vclick: function() {
-                        self._updateAll();
-                      },
+		this.refresh();
 
-                      focus: function() {
-                        label.addClass("ui-focus");
-                      },
+	},
 
-                      blur: function() {
-                        label.removeClass("ui-focus");
-                      }
-                    });
+	_cacheVals: function(){
+		this._getInputSet().each(function(){
+			$(this).jqmData("cacheVal", $(this).is(":checked") );
+		});
+	},
 
-      this.refresh();
+	//returns either a set of radios with the same name attribute, or a single checkbox
+	_getInputSet: function(){
+		return this.element.closest( "form,fieldset,:jqmData(role='page')" )
+				.find( "input[name='"+ this.element.attr( "name" ) +"'][type='"+ this.inputtype +"']" );
+	},
 
-    },
+	_updateAll: function(){
+		var self = this;
 
-    _cacheVals: function() {
-      this._getInputSet().each(function() {
-        $(this).jqmData("cacheVal", $(this).is(":checked"));
-      });
-    },
+		this._getInputSet().each(function(){
+			if( $(this).is(":checked") || self.inputtype === "checkbox" ){
+				$(this).trigger("change");
+			}
+		})
+		.checkboxradio( "refresh" );
+	},
 
-    //returns either a set of radios with the same name attribute, or a single checkbox
-    _getInputSet: function() {
-      return this.element.closest("form,fieldset,:jqmData(role='page')")
-              .find("input[name='" + this.element.attr("name") + "'][type='" + this.inputtype + "']");
-    },
+	refresh: function( ){
+		var input = this.element,
+			label = this.label,
+			icon = label.find( ".ui-icon" );
 
-    _updateAll: function() {
-      var self = this;
+		if ( input[0].checked ) {
+			label.addClass( $.mobile.activeBtnClass );
+			icon.addClass( this.checkedicon ).removeClass( this.uncheckedicon );
 
-      this._getInputSet().each(function() {
-        if ($(this).is(":checked") || self.inputtype === "checkbox") {
-          $(this).trigger("change");
-        }
-      })
-              .checkboxradio("refresh");
-    },
+		} else {
+			label.removeClass( $.mobile.activeBtnClass );
+			icon.removeClass( this.checkedicon ).addClass( this.uncheckedicon );
+		}
 
-    refresh: function() {
-      var input = this.element,
-              label = this.label,
-              icon = label.find(".ui-icon");
+		if( input.is( ":disabled" ) ){
+			this.disable();
+		}
+		else {
+			this.enable();
+		}
+	},
 
-      // input[0].checked expando doesn't always report the proper value
-      // for checked='checked'
-      if ($(input[0]).attr('checked')) {
-        label.addClass($.mobile.activeBtnClass);
-        icon.addClass(this.checkedicon).removeClass(this.uncheckedicon);
+	disable: function(){
+		this.element.attr("disabled",true).parent().addClass("ui-disabled");
+	},
 
-      } else {
-        label.removeClass($.mobile.activeBtnClass);
-        icon.removeClass(this.checkedicon).addClass(this.uncheckedicon);
-      }
-
-      if (input.is(":disabled")) {
-        this.disable();
-      }
-      else {
-        this.enable();
-      }
-    },
-
-    disable: function() {
-      this.element.attr("disabled", true).parent().addClass("ui-disabled");
-    },
-
-    enable: function() {
-      this.element.attr("disabled", false).parent().removeClass("ui-disabled");
-    }
-  });
-})(jQuery);
+	enable: function(){
+		this.element.attr("disabled",false).parent().removeClass("ui-disabled");
+	}
+});
+})( jQuery );
--- src/mobile/js/jquery.mobile.listview.js
+++ src/mobile/js/jquery.mobile.listview.js
@@ -1,231 +1,247 @@
 /*
- * jQuery Mobile Framework : "listview" plugin
- * Copyright (c) jQuery Project
- * Dual licensed under the MIT or GPL Version 2 licenses.
- * http://jquery.org/license
- */
-(function($, undefined) {
+* jQuery Mobile Framework : "listview" plugin
+* Copyright (c) jQuery Project
+* Dual licensed under the MIT or GPL Version 2 licenses.
+* http://jquery.org/license
+*/
+(function($, undefined ) {
 
-  $.widget("mobile.listview", $.mobile.widget, {
-    options: {
-      theme: "c",
-      countTheme: "c",
-      headerTheme: "b",
-      dividerTheme: "b",
-      splitIcon: "arrow-r",
-      splitTheme: "b",
-      inset: false
-    },
+$.widget( "mobile.listview", $.mobile.widget, {
+	options: {
+		theme: "c",
+		countTheme: "c",
+		headerTheme: "b",
+		dividerTheme: "b",
+		splitIcon: "arrow-r",
+		splitTheme: "b",
+		inset: false
+	},
+	
+	_create: function() {
+		var $list = this.element,
+			o = this.options;
 
-    _create: function() {
-      var t = this;
+		// create listview markup 
+		$list
+			.addClass( "ui-listview" );
+		
+		if ( o.inset ) {
+			$list.addClass( "ui-listview-inset ui-corner-all ui-shadow" );
+		}
 
-      // create listview markup
-      t.element.addClass(function(i, orig) {
-        return orig + " ui-listview " + ( t.options.inset ? " ui-listview-inset ui-corner-all ui-shadow " : "" );
-      });
+		this._itemApply( $list, $list );
+		
+		this.refresh( true );
 
-      t.refresh();
-    },
+	},
 
-    _itemApply: function($list, item) {
-      // TODO class has to be defined in markup
-      item.find(".ui-li-count")
-              .addClass("ui-btn-up-" + ($list.jqmData("counttheme") || this.options.countTheme) + " ui-btn-corner-all").end()
-              .find("h1, h2, h3, h4, h5, h6").addClass("ui-li-heading").end()
-              .find("p, dl").addClass("ui-li-desc").end()
-              .find("img:first-child:eq(0)").addClass("ui-li-thumb").each(
-              function() {
-                item.addClass($(this).is(".ui-li-icon") ? "ui-li-has-icon" : "ui-li-has-thumb");
-              }).end()
-              .find(".ui-li-aside").each(function() {
-        var $this = $(this);
-        $this.prependTo($this.parent()); //shift aside to front for css float
-      });
-    },
+	_itemApply: function( $list, item ) {
+		// TODO class has to be defined in markup
+		item.find( ".ui-li-count" )
+			.addClass( "ui-btn-up-" + ($list.jqmData( "counttheme" ) || this.options.countTheme) + " ui-btn-corner-all" );
 
-    _removeCorners: function(li) {
-      li
-              .add(li.find(".ui-btn-inner, .ui-li-link-alt, .ui-li-thumb"))
-              .removeClass("ui-corner-top ui-corner-bottom ui-corner-br ui-corner-bl ui-corner-tr ui-corner-tl");
-    },
+		item.find( "h1, h2, h3, h4, h5, h6" ).addClass( "ui-li-heading" );
 
-    refresh: function(create) {
-      this._createSubPages();
+		item.find( "p, dl" ).addClass( "ui-li-desc" );
 
-      var o = this.options,
-              $list = this.element,
-              self = this,
-              dividertheme = $list.jqmData("dividertheme") || o.dividerTheme,
-              listsplittheme = $list.jqmData("splittheme"),
-              listspliticon = $list.jqmData("spliticon"),
-              li = $list.children("li"),
-              counter = $.support.cssPseudoElement || !$.nodeName($list[0], "ol") ? 0 : 1;
+		$list.find( "li" ).find( ">img:eq(0), >:first>img:eq(0)" ).addClass( "ui-li-thumb" ).each(function() {
+			$( this ).closest( "li" ).addClass( $(this).is( ".ui-li-icon" ) ? "ui-li-has-icon" : "ui-li-has-thumb" );
+		});
 
-      if (counter) {
-        $list.find(".ui-li-dec").remove();
-      }
+		var aside = item.find( ".ui-li-aside" );
 
-      for (var pos = 0, numli = li.length; pos < numli; pos++) {
-        var item = li.eq(pos),
-                itemClass = "ui-li";
+		if ( aside.length ) {
+            aside.each(function(i, el) {
+			    $(el).prependTo( $(el).parent() ); //shift aside to front for css float
+            });
+		}
 
-        // If we're creating the element, we update it regardless
-        if (!create && item.hasClass("ui-li")) {
-          continue;
-        }
+		if ( $.support.cssPseudoElement || !$.nodeName( item[0], "ol" ) ) {
+			return;
+		}
+	},
+	
+	_removeCorners: function(li){
+		li
+			.add( li.find(".ui-btn-inner, .ui-li-link-alt, .ui-li-thumb") )
+			.removeClass( "ui-corner-top ui-corner-bottom ui-corner-br ui-corner-bl ui-corner-tr ui-corner-tl" );
+	},
+	
+	refresh: function( create ) {
+		this._createSubPages();
+		
+		var o = this.options,
+			$list = this.element,
+			self = this,
+			dividertheme = $list.jqmData( "dividertheme" ) || o.dividerTheme,
+			li = $list.children( "li" ),
+			counter = $.support.cssPseudoElement || !$.nodeName( $list[0], "ol" ) ? 0 : 1;
 
-        var itemTheme = item.jqmData("theme") || o.theme,
-                a = item.children("a");
+		if ( counter ) {
+			$list.find( ".ui-li-dec" ).remove();
+		}
 
-        if (a.length) {
-          var icon = item.jqmData("icon");
+		li.each(function( pos ) {
+			var item = $( this ),
+				itemClass = "ui-li";
 
-          item
-                  .buttonMarkup({
-                                  wrapperEls: "div",
-                                  shadow: false,
-                                  corners: false,
-                                  iconpos: "right",
-                                  icon: a.length > 1 || icon === false ? false : icon || "arrow-r",
-                                  theme: itemTheme
-                                });
+			// If we're creating the element, we update it regardless
+			if ( !create && item.hasClass( "ui-li" ) ) {
+				return;
+			}
 
-          a.first().addClass("ui-link-inherit");
+			var itemTheme = item.jqmData("theme") || o.theme;
 
-          if (a.length > 1) {
-            itemClass += " ui-li-has-alt";
+			var a = item.find( ">a" );
+				
+			if ( a.length ) {	
+				var icon = item.jqmData("icon");
+				
+				item
+					.buttonMarkup({
+						wrapperEls: "div",
+						shadow: false,
+						corners: false,
+						iconpos: "right",
+						icon: a.length > 1 || icon === false ? false : icon || "arrow-r",
+						theme: itemTheme
+					});
 
-            var last = a.last(),
-                    splittheme = listsplittheme || last.jqmData("theme") || o.splitTheme;
+				a.first().addClass( "ui-link-inherit" );
 
-            last
-                    .appendTo(item)
-                    .attr("title", last.text())
-                    .addClass("ui-li-link-alt")
-                    .empty()
-                    .buttonMarkup({
-                                    shadow: false,
-                                    corners: false,
-                                    theme: itemTheme,
-                                    icon: false,
-                                    iconpos: false
-                                  })
-                    .find(".ui-btn-inner")
-                    .append($("<span />").buttonMarkup({
-              shadow: true,
-              corners: true,
-              theme: splittheme,
-              iconpos: "notext",
-              icon: listspliticon || last.jqmData("icon") || o.splitIcon
-            }));
-          }
+				if ( a.length > 1 ) {
+					itemClass += " ui-li-has-alt";
 
-        } else if (item.jqmData("role") === "list-divider") {
-          itemClass += " ui-li-divider ui-btn ui-bar-" + dividertheme;
-          item.attr("role", "heading");
+					var last = a.last(),
+						splittheme = $list.jqmData( "splittheme" ) || last.jqmData( "theme" ) || o.splitTheme;
+					
+					last
+						.appendTo(item)
+						.attr( "title", last.text() )
+						.addClass( "ui-li-link-alt" )
+						.empty()
+						.buttonMarkup({
+							shadow: false,
+							corners: false,
+							theme: itemTheme,
+							icon: false,
+							iconpos: false
+						})
+						.find( ".ui-btn-inner" )
+							.append( $( "<span>" ).buttonMarkup({
+								shadow: true,
+								corners: true,
+								theme: splittheme,
+								iconpos: "notext",
+								icon: $list.jqmData( "spliticon" ) || last.jqmData( "icon" ) ||  o.splitIcon
+							} ) );
+				}
 
-          //reset counter when a divider heading is encountered
-          if (counter) {
-            counter = 1;
-          }
+			} else if ( item.jqmData( "role" ) === "list-divider" ) {
+				itemClass += " ui-li-divider ui-btn ui-bar-" + dividertheme;
+				item.attr( "role", "heading" );
 
-        } else {
-          itemClass += " ui-li-static ui-body-" + itemTheme;
-        }
+				//reset counter when a divider heading is encountered
+				if ( counter ) {
+					counter = 1;
+				}
+
+			} else {
+				itemClass += " ui-li-static ui-body-" + itemTheme;
+			}
+			
+			
+			if( o.inset ){	
+				if ( pos === 0 ) {
+						itemClass += " ui-corner-top";
+	
+						item
+							.add( item.find( ".ui-btn-inner" ) )
+							.find( ".ui-li-link-alt" )
+								.addClass( "ui-corner-tr" )
+							.end()
+							.find( ".ui-li-thumb" )
+								.addClass( "ui-corner-tl" );
+						if(item.next().next().length){
+							self._removeCorners( item.next() );		
+						}
+	
+				}
+				if ( pos === li.length - 1 ) {
+						itemClass += " ui-corner-bottom";
+	
+						item
+							.add( item.find( ".ui-btn-inner" ) )
+							.find( ".ui-li-link-alt" )
+								.addClass( "ui-corner-br" )
+							.end()
+							.find( ".ui-li-thumb" )
+								.addClass( "ui-corner-bl" );
+						
+						if(item.prev().prev().length){
+							self._removeCorners( item.prev() );		
+						}	
+				}
+			}
 
 
-        if (o.inset) {
-          if (pos === 0) {
-            itemClass += " ui-corner-top";
+			if ( counter && itemClass.indexOf( "ui-li-divider" ) < 0 ) {
+			
+				var countParent = item.is(".ui-li-static:first") ? item : item.find( ".ui-link-inherit" );
+				
+				countParent
+					.addClass( "ui-li-jsnumbering" )
+					.prepend( "<span class='ui-li-dec'>" + (counter++) + ". </span>" );
+			}
 
-            item
-                    .add(item.find(".ui-btn-inner"))
-                    .find(".ui-li-link-alt")
-                    .addClass("ui-corner-tr")
-                    .end()
-                    .find(".ui-li-thumb")
-                    .addClass("ui-corner-tl");
-            if (item.next().next().length) {
-              self._removeCorners(item.next());
-            }
+			item.add( item.find( ".ui-btn-inner" ) ).addClass( itemClass );
 
-          }
-          if (pos === li.length - 1) {
-            itemClass += " ui-corner-bottom";
+			if ( !create ) {
+				self._itemApply( $list, item );
+			}
+		});
+	},
+	
+	//create a string for ID/subpage url creation
+	_idStringEscape: function( str ){
+		return str.replace(/[^a-zA-Z0-9]/g, '-');
+	},
+	
+	_createSubPages: function() {
+		var parentList = this.element,
+			parentPage = parentList.closest( ".ui-page" ),
+			parentId = parentPage.jqmData( "url" ),
+			o = this.options,
+			self = this,
+			persistentFooterID = parentPage.find( ":jqmData(role='footer')" ).jqmData( "id" );
 
-            item
-                    .add(item.find(".ui-btn-inner"))
-                    .find(".ui-li-link-alt")
-                    .addClass("ui-corner-br")
-                    .end()
-                    .find(".ui-li-thumb")
-                    .addClass("ui-corner-bl");
+		$( parentList.find( "li>ul, li>ol" ).toArray().reverse() ).each(function( i ) {
+			var list = $( this ),
+				parent = list.parent(),
+				nodeEls = $( list.prevAll().toArray().reverse() ),
+				nodeEls = nodeEls.length ? nodeEls : $( "<span>" + $.trim(parent.contents()[ 0 ].nodeValue) + "</span>" ),
+				title = nodeEls.first().text(),//url limits to first 30 chars of text
+				id = parentId + "&" + $.mobile.subPageUrlKey + "=" + self._idStringEscape(title + " " + i),
+				theme = list.jqmData( "theme" ) || o.theme,
+				countTheme = list.jqmData( "counttheme" ) || parentList.jqmData( "counttheme" ) || o.countTheme,
+				newPage = list.wrap( "<div data-" + $.mobile.ns + "role='page'><div data-" + $.mobile.ns + "role='content'></div></div>" )
+							.parent()
+								.before( "<div data-" + $.mobile.ns + "role='header' data-" + $.mobile.ns + "theme='" + o.headerTheme + "'><div class='ui-title'>" + title + "</div></div>" )
+								.after( persistentFooterID ? $( "<div data-" + $.mobile.ns + "role='footer'  data-" + $.mobile.ns + "id='"+ persistentFooterID +"'>") : "" )
+								.parent()
+									.attr( "data-" + $.mobile.ns + "url", id )
+									.attr( "data-" + $.mobile.ns + "theme", theme )
+									.attr( "data-" + $.mobile.ns + "count-theme", countTheme )
+									.appendTo( $.mobile.pageContainer );
 
-            if (item.prev().prev().length) {
-              self._removeCorners(item.prev());
-            }
-          }
-        }
+				newPage.page();		
+			var anchor = parent.find('a:first');
+			if (!anchor.length) {
+				anchor = $("<a></a>").html( nodeEls || title ).prependTo(parent.empty());
+			}
+			anchor.attr('href','#' + id);
+		}).listview();
+	}
+});
 
-
-        if (counter && itemClass.indexOf("ui-li-divider") < 0) {
-
-          var countParent = item.is(".ui-li-static:first") ? item : item.find(".ui-link-inherit");
-
-          countParent
-                  .addClass("ui-li-jsnumbering")
-                  .prepend("<span class='ui-li-dec'>" + (counter++) + ". </span>");
-        }
-
-        item.add(item.find(".ui-btn-inner")).addClass(itemClass);
-
-        if (!create) {
-          self._itemApply($list, item);
-        }
-      }
-    },
-
-    //create a string for ID/subpage url creation
-    _idStringEscape: function(str) {
-      return str.replace(/[^a-zA-Z0-9]/g, '-');
-    },
-
-    _createSubPages: function() {
-      var parentList = this.element,
-              parentPage = parentList.closest(".ui-page"),
-              parentId = parentPage.jqmData("url"),
-              o = this.options,
-              dns = "data-" + $.mobile.ns,
-              self = this,
-              persistentFooterID = parentPage.find(":jqmData(role='footer')").jqmData("id");
-
-      $(parentList.find("li>ul, li>ol").toArray().reverse()).each(
-              function(i) {
-                var list = $(this),
-                        parent = list.parent(),
-                        nodeEls = $(list.prevAll().toArray().reverse()),
-                        nodeEls = nodeEls.length ? nodeEls : $("<span>" + $.trim(parent.contents()[ 0 ].nodeValue) + "</span>"),
-                        title = nodeEls.first().text(),//url limits to first 30 chars of text
-                        id = parentId + "&" + $.mobile.subPageUrlKey + "=" + self._idStringEscape(title + " " + i),
-                        theme = list.jqmData("theme") || o.theme,
-                        countTheme = list.jqmData("counttheme") || parentList.jqmData("counttheme") || o.countTheme,
-                        newPage = list.detach()
-                                .wrap("<div " + dns + "role='page'" + dns + "url='" + id + "' " + dns + "theme='" + theme + "' " + dns + "count-theme='" + countTheme + "'><div " + dns + "role='content'></div></div>")
-                                .parent()
-                                .before("<div " + dns + "role='header' " + dns + "theme='" + o.headerTheme + "'><div class='ui-title'>" + title + "</div></div>")
-                                .after(persistentFooterID ? $("<div " + dns + "role='footer' " + dns + "id='" + persistentFooterID + "'>") : "")
-                                .parent()
-                                .appendTo($.mobile.pageContainer);
-
-                newPage.page();
-                var anchor = parent.find('a:first');
-                if (!anchor.length) {
-                  anchor = $("<a />").html(nodeEls || title).prependTo(parent.empty());
-                }
-                anchor.attr('href', '#' + id);
-              }).listview();
-    }
-  });
-
-})(jQuery);
+})( jQuery );
