9bb76bfa5c786a2fe06eda4142698a2bcb9089ed
kosmas58 <kosmas.schuetz@gmx.com>
2011-05-22 10:21:48 +0200

    Minor fix
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- templates/markitup/markitup/jquery.markitup.set.textile.js
+++ templates/markitup/markitup/jquery.markitup.set.textile.js
@@ -55,25 +55,3 @@ mySettings = {
 		{name:'Preview', call:'preview', className:'preview'}
 	]
 }
-
-
-mySettings = {
-	markupSet:  [
-	{	name:'Table generator',
-		className:'tablegenerator',
-		placeholder:"Your text here...",
-		replaceWith:function(h) {
-			cols = prompt("How many cols?");
-			rows = prompt("How many rows?");
-			html = "";
-			for (r = 0; r < rows; r++) {
-				for (c = 0; c < cols; c++) {
-					html += "|"+(h.placeholder||"");
-				}
-				html += "|\n";
-			}
-			return html;
-		}
-	}
-	]
-}
