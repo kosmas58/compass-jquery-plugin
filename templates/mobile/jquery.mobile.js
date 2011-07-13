9eabb31d236796eb78475b3d6c4026e8b4ed4217
kosmas58 <kosmas.schuetz@gmx.com>
2011-07-13 19:52:37 +0200

    Updated jQuery Mobile
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- templates/mobile/jquery.mobile.js
+++ templates/mobile/jquery.mobile.js
@@ -5036,7 +5036,7 @@ $.widget( "mobile.slider", $.mobile.widget, {
 				}
 			});
 
-		this.refresh();
+		this.refresh(undefined, undefined, true);
 	},
 
 	refresh: function( val, isfromControl, preventInputUpdate ) {
