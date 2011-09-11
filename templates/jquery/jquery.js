6019fbbe1cca5b90d3c2a22a46f89ea052d82495
kosmas58 <kosmas.schuetz@gmx.com>
2011-09-11 11:39:37 +0200

    Rebuilt gem
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- templates/jquery/jquery.js
+++ templates/jquery/jquery.js
@@ -11,7 +11,7 @@
  * Copyright 2011, The Dojo Foundation
  * Released under the MIT, BSD, and GPL Licenses.
  *
- * Date: Sun Sep 11 11:31:51 +0200 2011
+ * Date: Sun Sep 11 11:33:59 +0200 2011
  */
 (function( window, undefined ) {
 
